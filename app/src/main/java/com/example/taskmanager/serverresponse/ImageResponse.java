521e73da1ba651e4c8c7181bc512aa4ce135b03d
bihwal <strength147@gmail.com>
2020-01-02 08:52:39 +0545

    Initial commit

---
+++ app/src/main/java/com/example/taskmanager/serverresponse/ImageResponse.java
@@ -0,0 +1,28 @@
+package com.example.taskmanager.serverresponse;
+
+public class ImageResponse {
+
+    private String status;
+    private String token;
+
+    public ImageResponse(String status, String token) {
+        this.status = status;
+        this.token = token;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
